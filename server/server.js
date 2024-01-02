5a5bc170a8a2df75ef19cb8240f47356735a5500
dorian2023 <doriangonzalez2018@gmail.com>
2024-01-02 19:03:25 -0400

    Primer Commit integrando todos los archivos

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// const routerApp = require('./router/index.js');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Ready!!!!!!!!!!!!!!!');
+})
+
+// routerApp(app);
+
+app.listen(3100, () => {
+    console.log('Servidor arrancando');
+})
+
+
+
+
+// {}
