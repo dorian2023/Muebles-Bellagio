0a2e926abd688c0ff85c014448f3caf41d64daf7
dorian2023 <doriangonzalez2018@gmail.com>
2024-01-04 14:25:14 -0400

    algnas configuraciones a la base de datos donde no responde el fetch

--- client/products/product.js
+++ client/products/product.js
@@ -2,14 +2,17 @@ const productsList = document.getElementById('productList');
 const btnAddProduct = document.getElementById('btnAddProduct');
 const loaderDiv = document.getElementById('loader');
 
+//AQUI MUESTRA ENESTA RUTA EL OBJETO JSON CON TODOS LOS PRODUCTOS DE LA BASE DE DATOS
 const printProductList = async () =>{
-    const dataList = await fetch('http://localhost:3100/products', { method: 'GET'})
+    const dataList = await fetch('http://localhost:3100/products', { method: 'GET',});
     const dataResult = await dataList.json();
     console.log(dataResult);
-    return dataResult
+    return dataResult;
 }
 
+//AQUI IMPREME EN ESTA MRUTA ESE MISMO CONTENDO EN EL FRONTEND
 document.addEventListener('DOMContentLoaded', () => {
+
     printProductList()
         .then((products) => {
 
@@ -17,26 +20,28 @@ document.addEventListener('DOMContentLoaded', () => {
                 return loaderDiv.innerHTML = `<h2>Cargando...</h2>`;
             } else {
                 const listProducts = products.message.map(products =>{
+
                     function updateProduct() {
-                        return window.location.href = './updateproduct.html'
+                        return window.location.href = './updateproduct.html';ç
                     }
 
-                const buttons = () => `
+                    const buttons = () => `
                         <button id="test" onclick="${updateProduct}">Actualizar Product</button>
                         <button id="btnDeleteProduct">Eliminar Producto</button>
-                    `
-                const printProduct = `
-                    <ul clas="listProduct">
-                        <li>Title: ${products.product.title}</li>
-                        <li>Descipcion: ${products.product.description}</li>  
-                        <img src="${products.product.image}" />
-                    </u>
-                    ${buttons()}
-                `;
-                return printProduct;
+                        `
+                    const printProduct = `
+                        <ul clas="listProduct">
+                            <li>Title: ${products.product.title}</li>
+                            <li>Descipcion: ${products.product.description}</li>  
+                            <li>Descipcion: ${products.product.price}</li>  
+                            <img src="${products.product.image}" />
+                        </ul>
+                        ${buttons()}
+                    `;
+                    return printProduct;
 
                 });
-                productsList.innerHTML = listProducts.join("");
+                productList.innerHTML = listProducts.join("");
             }
         })
         .catch((error) => console.error(error));
