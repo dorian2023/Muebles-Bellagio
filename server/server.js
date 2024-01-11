c4f9570af6931b92fe1bf4311e9506a2b2a41976
dorian2023 <doriangonzalez2018@gmail.com>
2024-01-11 17:31:47 -0400

    inicio en la pag index

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname,'client')))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 routerApp(app);
